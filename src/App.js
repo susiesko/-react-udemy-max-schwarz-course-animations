c9d38dd7b956e52140a54b6cf80cd72a6c1f765d
Susie <susie.sko@gmail.com>
2021-02-07 14:14:00 -0800

    after lecture 401

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from 'react-transition-group/Transition';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -8,6 +9,7 @@ import List from "./components/List/List";
 class App extends Component {
   state = { 
     modalIsOpen: false,
+    showBlock: false,
     isInit: true
   }
 
@@ -19,13 +21,56 @@ class App extends Component {
     this.setState({ modalIsOpen: false });
   }
 
+  toggleClick = () => {
+    this.setState(prevState => ({ showBlock: !prevState.showBlock }))
+  }
+
+  blockTransitionHandler = state => {
+    const blockStyle = {
+      backgroundColor: 'red', 
+      width: 100, 
+      height: 100, 
+      margin: 'auto',
+      opacity: state === 'exiting' || state === 'entering' ? 0 : 1,
+      transition: 'opacity 500ms ease-out'
+    };
+
+    return <div style={blockStyle}>{state}</div>;
+  }
+
   render() {
+    const blockStyle = {
+      backgroundColor: 'red', 
+      width: 100, 
+      height: 100, 
+      margin: 'auto'
+    };
+
+    //let block = <div style={blockStyle}></div>;
+
+    const block = (
+      <Transition 
+        in={this.state.showBlock}
+        timeout={1000}
+        mouseOnEnter
+        unmountOnExit
+      >
+        { this.blockTransitionHandler }
+      </Transition>
+    );
+
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <p>
+          <button onClick={this.toggleClick}>Toggle</button>
+        </p>
+        { block }
+
         { this.state.modalIsOpen 
           ? <Modal show={this.state.modalIsOpen} isInit={this.state.isInit} closed={this.closeModal} /> 
           : null}
+
         { this.state.modalIsOpen 
           ? <Backdrop show={this.state.modalIsOpen} onClick={this.closeModal} />
           : null}        
